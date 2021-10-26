9aa3c120682b9fa87f5555ca3acb039992258321
tmahere <80914897+tmahere@users.noreply.github.com>
2021-10-26 04:25:30 -0400

    Add project files.

---
+++ BulletTrainHMI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BulletTrainHMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
