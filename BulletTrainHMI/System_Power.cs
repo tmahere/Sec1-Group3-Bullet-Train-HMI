b5e8e20e7eac572eac5e92ea26b1c92ae7aa3d76
DPham <dom17pham@gmail.com>
2021-11-18 16:29:02 -0500

    Renamed system power module class to "system power", Added functions to system power class, and separated the nested classes into individual class files.

---
+++ BulletTrainHMI/System_Power.cs
@@ -0,0 +1,55 @@
+﻿//  System Power Module
+//
+//  This module monitors the power quality of the train's power supply and controls the connection between the train 
+//  and the overhead power supply line 
+//
+// Version 1.1
+//
+// Version History
+// 1.0              Initial version 
+// 1.1              Renamed system power module class to "system power", Added functions to system power class, and
+//                  separated the nested classes in system power class from previous version into individual class files for better organization. 
+//
+// Written by Dominic Pham
+
+namespace BulletTrainHMI
+{
+    public class System_Power
+    {
+        pantograph SYSTEM_PANTOGRAPH;
+        voltage SYSTEM_VOLTAGE;
+        current SYSTEM_CURRENT;
+
+        public System_Power()
+        {
+            SYSTEM_PANTOGRAPH = new pantograph();
+            SYSTEM_VOLTAGE = new voltage();
+            SYSTEM_CURRENT = new current();
+        }
+
+        public bool Get_System_Pantograph()
+        {
+            return SYSTEM_PANTOGRAPH.pantograph_status();
+        }
+
+        public float Get_System_Voltage()
+        {
+            return SYSTEM_VOLTAGE.Get_Voltage();
+        }
+
+        public float Get_System_Current()
+        {
+            return SYSTEM_CURRENT.Get_Current();
+        }
+
+        public void Raise_System_Pantograph()
+        {
+            SYSTEM_PANTOGRAPH.pantograph_raise();
+        }
+
+        public void Lower_System_Pantograph()
+        {
+            SYSTEM_PANTOGRAPH.pantograph_lower();
+        }
+    }
+}
---
+++ BulletTrainHMI/voltage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace BulletTrainHMI
+{
+    class voltage
+    {
+        float Voltage;                      // variable to represent the voltage reading, ratings are in kV 
+        bool Voltage_Over;                  // variable to test for overvoltage condition within the power supply
+        bool Voltage_Under;                 // variable to test for undervoltage condition within the power supply
+        public voltage()                    // default constructor 
+        {
+            Voltage = 0;
+            Voltage_Over = false;
+            Voltage_Under = false;
+        }
+
+        public float Get_Voltage()          // gets the voltage reading from voltage rating file
+        {
+            try
+            {
+                using (StreamReader FILE = new StreamReader(@"C:\Users\dom17\Desktop\PROJ3-Bullet-Train\tmahere\Sec1-Group3-Bullet-Train-HMI\BulletTrainHMI\voltage ratings.txt")) // file location will change accordingly when the module is integrated
+                {
+                    Random rnd = new Random();              // random number generator used to create a line index to read from
+                    int lineIndex = rnd.Next(1, File.ReadLines(@"C:\Users\dom17\Desktop\PROJ3-Bullet-Train\tmahere\Sec1-Group3-Bullet-Train-HMI\BulletTrainHMI\voltage ratings.txt").Count());
+                    for (int a = 1; a < lineIndex; a++)
+                    {                                       // this loop will read through the file until it reaches the line index that was created by rnd  
+                        FILE.ReadLine();
+                    }
+                    Voltage = float.Parse(FILE.ReadLine()); // converts the string to a float value 
+                    Voltage_Over = Over_Voltage(Voltage);   // checks the voltage rating for overvoltage condition
+                    Voltage_Under = Under_Voltage(Voltage); // checks the voltage rating for undervoltage condition
+                    if ((Voltage_Over == false) && (Voltage_Under == false))
+                    {
+                        return Voltage;
+                    }
+                    else
+                    {
+                        if (Voltage_Over == true)
+                        {
+                            Console.WriteLine("\tSystem Power Warning: OVERVOLTAGE, ");
+                            return Voltage;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\tSystem Power Warning: UNDERVOLTAGE, ");
+                            return Voltage;
+                        }
+                    }
+
+                }
+            }
+            catch // catch condition if any issues arise when attempting to read the voltage rating file 
+            {
+                Console.WriteLine("Unable to read voltage.");
+                return Voltage = 0;
+            }
+        }
+
+        public bool Under_Voltage(float VOLTAGE)
+        {
+            if (VOLTAGE < 17.5) // 17.5 kV is the standard minimum non permanent voltage for a 50 Hz railway electricfication system 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Over_Voltage(float VOLTAGE)
+        {
+            if (VOLTAGE > 29) // 29 kV is the standard maximum non permanent voltage for a 50 Hz railway electricfication system
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
