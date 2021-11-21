e77824ca96ba55717a08a684bbdd74a46d278205
Lightningale <gyl.lakewood@gmail.com>
2021-11-21 16:23:51 -0500

    Implemented tempManagerClass
    passed integration tests
    T_INT_IM01_READ_FLOAT,
    T_INT_IM02_SET_TEMP,
    T_INT_IM03_GET_TEMP

--- BulletTrainHMI/Program.cs
+++ BulletTrainHMI/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using System.IO;
+using System.Threading.Tasks;
 namespace BulletTrainHMI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TempManager tempManager = new TempManager(Path.Combine(Environment.CurrentDirectory,@"data\","tempData.txt"));
+            tempManager.setTemp(26);
+            for(int i=1;i<=100;i++)
+            {
+                tempManager.readData(i);
+                Console.WriteLine("{0:F}\n", tempManager.getTemp());
+                tempManager.airConditioning();
+               // await Task.Delay(1000);
+            }
+            Console.WriteLine("finished\n");
         }
     }
 }
---
+++ BulletTrainHMI/TempManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletTrainHMI
+{
+    class TempManager:ComponentManager
+    {
+        string filePath;
+        float destTemp;
+        float currentTemp;
+
+        public TempManager(string path)
+        {
+            filePath = path;
+            //StreamReader reader = new StreamReader(filePath);
+            currentTemp = float.Parse(File.ReadLines(filePath).Take(1).First());
+            Console.WriteLine("initialize: {0:F}\n",currentTemp);
+        }
+        public void readData(int line)
+        {
+            currentTemp += float.Parse(File.ReadLines(filePath).Skip(line).Take(1).First());
+            /*StreamReader reader = new StreamReader(filePath);
+            for (int i = 0; i < line; i++)
+                reader.ReadLine();
+            currentTemp = float.Parse(reader.ReadLine());*/
+            //currentTemp = float.Parse(reader.ReadLine().Skip(line-1).Take(1).First());
+        }
+        public float getTemp()
+        {
+            return currentTemp;
+        }
+        public void setTemp(float temp)
+        {
+            destTemp = temp;
+        }
+        public void airConditioning()
+        {
+            if(currentTemp<destTemp)
+            {
+                currentTemp += 0.1f;
+            }
+            else if(currentTemp>destTemp)
+            {
+                currentTemp -= 0.1f;
+            }
+        }
+    }
+}
