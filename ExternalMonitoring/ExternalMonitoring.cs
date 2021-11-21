de70059bba1e6e1ae2670ea34c117b622f28da10
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2021-11-21 04:29:53 -0500

    Created GPS functions.

--- ExternalMonitoring/ExternalMonitoring.cs
+++ ExternalMonitoring/ExternalMonitoring.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+// this module should connect to the Radio and GPS classes
+// the module will interact with the UI module
+// connect to the driving module if needed
 namespace ExternalMonitoring
 {
     class ExternalMonitoring
