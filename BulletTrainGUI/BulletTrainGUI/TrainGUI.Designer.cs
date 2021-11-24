511790b29e8728bf7e738b3791c17c33b0978d9a
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2021-11-24 09:14:46 -0500

    Added the WinForms file  and UI shell. Included notes on integrating in the source code.

---
+++ BulletTrainGUI/BulletTrainGUI/TrainGUI.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace BulletTrainGUI
+{
+    partial class TrainGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrainGUI));
+            this.axGlg1 = new AxGlgoleLib.AxGlg();
+            this.axGlg2 = new AxGlgoleLib.AxGlg();
+            this.axGlg3 = new AxGlgoleLib.AxGlg();
+            this.axGlg4 = new AxGlgoleLib.AxGlg();
+            this.axGlg6 = new AxGlgoleLib.AxGlg();
+            this.axGlg7 = new AxGlgoleLib.AxGlg();
+            this.axGlg8 = new AxGlgoleLib.AxGlg();
+            this.axGlg10 = new AxGlgoleLib.AxGlg();
+            this.axGlg5 = new AxGlgoleLib.AxGlg();
+            this.axGlg13 = new AxGlgoleLib.AxGlg();
+            this.axGlg14 = new AxGlgoleLib.AxGlg();
+            this.axGlg15 = new AxGlgoleLib.AxGlg();
+            this.loopTimer = new System.Windows.Forms.Timer(this.components);
+            this.voltageLabel = new System.Windows.Forms.Label();
+            this.currentLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg15)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axGlg1
+            // 
+            this.axGlg1.Enabled = true;
+            this.axGlg1.Location = new System.Drawing.Point(21, 21);
+            this.axGlg1.Name = "axGlg1";
+            this.axGlg1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg1.OcxState")));
+            this.axGlg1.Size = new System.Drawing.Size(101, 183);
+            this.axGlg1.TabIndex = 0;
+            this.axGlg1.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg1_Input);
+            // 
+            // axGlg2
+            // 
+            this.axGlg2.Enabled = true;
+            this.axGlg2.Location = new System.Drawing.Point(21, 201);
+            this.axGlg2.Name = "axGlg2";
+            this.axGlg2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg2.OcxState")));
+            this.axGlg2.Size = new System.Drawing.Size(101, 74);
+            this.axGlg2.TabIndex = 1;
+            this.axGlg2.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg2_Input);
+            // 
+            // axGlg3
+            // 
+            this.axGlg3.Enabled = true;
+            this.axGlg3.Location = new System.Drawing.Point(21, 557);
+            this.axGlg3.Name = "axGlg3";
+            this.axGlg3.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg3.OcxState")));
+            this.axGlg3.Size = new System.Drawing.Size(392, 284);
+            this.axGlg3.TabIndex = 2;
+            // 
+            // axGlg4
+            // 
+            this.axGlg4.Enabled = true;
+            this.axGlg4.Location = new System.Drawing.Point(351, 109);
+            this.axGlg4.Name = "axGlg4";
+            this.axGlg4.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg4.OcxState")));
+            this.axGlg4.Size = new System.Drawing.Size(51, 92);
+            this.axGlg4.TabIndex = 3;
+            this.axGlg4.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg4_Input);
+            // 
+            // axGlg6
+            // 
+            this.axGlg6.Enabled = true;
+            this.axGlg6.Location = new System.Drawing.Point(21, 498);
+            this.axGlg6.Name = "axGlg6";
+            this.axGlg6.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg6.OcxState")));
+            this.axGlg6.Size = new System.Drawing.Size(392, 53);
+            this.axGlg6.TabIndex = 5;
+            // 
+            // axGlg7
+            // 
+            this.axGlg7.Enabled = true;
+            this.axGlg7.Location = new System.Drawing.Point(21, 301);
+            this.axGlg7.Name = "axGlg7";
+            this.axGlg7.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg7.OcxState")));
+            this.axGlg7.Size = new System.Drawing.Size(186, 168);
+            this.axGlg7.TabIndex = 6;
+            // 
+            // axGlg8
+            // 
+            this.axGlg8.Enabled = true;
+            this.axGlg8.Location = new System.Drawing.Point(227, 301);
+            this.axGlg8.Name = "axGlg8";
+            this.axGlg8.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg8.OcxState")));
+            this.axGlg8.Size = new System.Drawing.Size(186, 168);
+            this.axGlg8.TabIndex = 7;
+            // 
+            // axGlg10
+            // 
+            this.axGlg10.Enabled = true;
+            this.axGlg10.Location = new System.Drawing.Point(1100, 21);
+            this.axGlg10.Name = "axGlg10";
+            this.axGlg10.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg10.OcxState")));
+            this.axGlg10.Size = new System.Drawing.Size(293, 183);
+            this.axGlg10.TabIndex = 9;
+            this.axGlg10.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg10_Input);
+            // 
+            // axGlg5
+            // 
+            this.axGlg5.Enabled = true;
+            this.axGlg5.Location = new System.Drawing.Point(437, 21);
+            this.axGlg5.Name = "axGlg5";
+            this.axGlg5.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg5.OcxState")));
+            this.axGlg5.Size = new System.Drawing.Size(166, 180);
+            this.axGlg5.TabIndex = 11;
+            // 
+            // axGlg13
+            // 
+            this.axGlg13.Enabled = true;
+            this.axGlg13.Location = new System.Drawing.Point(686, 21);
+            this.axGlg13.Name = "axGlg13";
+            this.axGlg13.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg13.OcxState")));
+            this.axGlg13.Size = new System.Drawing.Size(163, 180);
+            this.axGlg13.TabIndex = 13;
+            this.axGlg13.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg13_Input);
+            // 
+            // axGlg14
+            // 
+            this.axGlg14.Enabled = true;
+            this.axGlg14.Location = new System.Drawing.Point(866, 21);
+            this.axGlg14.Name = "axGlg14";
+            this.axGlg14.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg14.OcxState")));
+            this.axGlg14.Size = new System.Drawing.Size(214, 183);
+            this.axGlg14.TabIndex = 14;
+            this.axGlg14.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg14_Input);
+            // 
+            // axGlg15
+            // 
+            this.axGlg15.Enabled = true;
+            this.axGlg15.Location = new System.Drawing.Point(154, 70);
+            this.axGlg15.Name = "axGlg15";
+            this.axGlg15.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg15.OcxState")));
+            this.axGlg15.Size = new System.Drawing.Size(160, 164);
+            this.axGlg15.TabIndex = 15;
+            // 
+            // loopTimer
+            // 
+            this.loopTimer.Tick += new System.EventHandler(this.loopTimer_Tick);
+            // 
+            // voltageLabel
+            // 
+            this.voltageLabel.AutoSize = true;
+            this.voltageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.voltageLabel.Location = new System.Drawing.Point(943, 209);
+            this.voltageLabel.Name = "voltageLabel";
+            this.voltageLabel.Size = new System.Drawing.Size(50, 25);
+            this.voltageLabel.TabIndex = 17;
+            this.voltageLabel.Text = "30.0";
+            this.voltageLabel.Click += new System.EventHandler(this.voltageLabel_Click);
+            // 
+            // currentLabel
+            // 
+            this.currentLabel.AutoSize = true;
+            this.currentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.currentLabel.Location = new System.Drawing.Point(1224, 209);
+            this.currentLabel.Name = "currentLabel";
+            this.currentLabel.Size = new System.Drawing.Size(50, 25);
+            this.currentLabel.TabIndex = 18;
+            this.currentLabel.Text = "40.1";
+            this.currentLabel.Click += new System.EventHandler(this.currentLabel_Click);
+            // 
+            // TrainGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1405, 853);
+            this.Controls.Add(this.currentLabel);
+            this.Controls.Add(this.voltageLabel);
+            this.Controls.Add(this.axGlg15);
+            this.Controls.Add(this.axGlg14);
+            this.Controls.Add(this.axGlg13);
+            this.Controls.Add(this.axGlg5);
+            this.Controls.Add(this.axGlg10);
+            this.Controls.Add(this.axGlg8);
+            this.Controls.Add(this.axGlg7);
+            this.Controls.Add(this.axGlg6);
+            this.Controls.Add(this.axGlg4);
+            this.Controls.Add(this.axGlg3);
+            this.Controls.Add(this.axGlg2);
+            this.Controls.Add(this.axGlg1);
+            this.Name = "TrainGUI";
+            this.Text = "Bullet Train HMI";
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg15)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxGlgoleLib.AxGlg axGlg1;
+        private AxGlgoleLib.AxGlg axGlg2;
+        private AxGlgoleLib.AxGlg axGlg3;
+        private AxGlgoleLib.AxGlg axGlg4;
+        private AxGlgoleLib.AxGlg axGlg6;
+        private AxGlgoleLib.AxGlg axGlg7;
+        private AxGlgoleLib.AxGlg axGlg8;
+        private AxGlgoleLib.AxGlg axGlg10;
+        private AxGlgoleLib.AxGlg axGlg5;
+        private AxGlgoleLib.AxGlg axGlg13;
+        private AxGlgoleLib.AxGlg axGlg14;
+        private AxGlgoleLib.AxGlg axGlg15;
+        private System.Windows.Forms.Timer loopTimer;
+        private System.Windows.Forms.Label voltageLabel;
+        private System.Windows.Forms.Label currentLabel;
+    }
+}
+
---
+++ BulletTrainGUI/BulletTrainGUI/TrainGUI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using GlgoleLib;
+using System.Windows.Forms;
+
+// don't change any of the method names because the form WILL break
+// to change it you must change the widget's name in properties FIRST then the method name
+
+/*
+How to simulate data:
+- add a timer from the toolbox that your label/widget will be linked to
+- the timer essentially refreshes that widget and updates it with the new data being read in
+    Use:
+    timerName.Interval = x;
+    timerName.Start();
+
+- .interval changes the speed of the change
+- in timerName_tick()
+    - call the function that will be sending in data
+
+To display numbers/strings to a label
+- use the data that is read in from your function
+ex.
+label.text = [function call];
+
+^^ some code modifying might be required
+*/
+
+namespace BulletTrainGUI
+{
+    public partial class TrainGUI : Form
+    {
+
+        public TrainGUI()
+        {
+            InitializeComponent();
+            loopTimer.Interval = 100;
+            loopTimer.Start();
+        }
+
+        private void axGlg1_Input(object sender, AxGlgoleLib._DGlgEvents_InputEvent e) // current thermometer
+        {
+            
+        }
+
+        private void axGlg2_Input(object sender, AxGlgoleLib._DGlgEvents_InputEvent e) // thermometer adjuster
+        {
+            
+        }
+
+        private void axGlg4_Input(object sender, AxGlgoleLib._DGlgEvents_InputEvent e) // radio - on/off
+        {
+            
+        }
+
+        private void axGlg10_Input(object sender, AxGlgoleLib._DGlgEvents_InputEvent e) // amps meter
+        {
+
+        }
+
+        private void axGlg14_Input(object sender, AxGlgoleLib._DGlgEvents_InputEvent e) // voltage meter
+        {
+
+        }
+
+        private void axGlg13_Input(object sender, AxGlgoleLib._DGlgEvents_InputEvent e) // lower/raise pantograph
+        {
+
+        }
+
+        private void loopTimer_Tick(object sender, EventArgs e) // example on how to simulate random data by looping through an array
+        {
+            float[] sample = { 40.3f, 41.2f, 41.6f, 42.4f, 45.1f, 44.7f, 43.9f, 43.2f, 41.7f, 41.9f, 41.6f };
+            Random rand = new Random();
+
+            currentLabel.Text = sample[rand.Next(0,11)].ToString();
+        }
+
+        private void voltageLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void currentLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
