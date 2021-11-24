90b72a4b538c5a2968362b8f53fc7be1a3d16df5
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2021-11-24 13:18:04 -0500

    Changed the system power graphics. Moved the formatting around a bit.

--- BulletTrainGUI/BulletTrainGUI/TrainGUI.Designer.cs
+++ BulletTrainGUI/BulletTrainGUI/TrainGUI.Designer.cs
@@ -39,13 +39,18 @@ namespace BulletTrainGUI
             this.axGlg7 = new AxGlgoleLib.AxGlg();
             this.axGlg8 = new AxGlgoleLib.AxGlg();
             this.axGlg10 = new AxGlgoleLib.AxGlg();
-            this.axGlg5 = new AxGlgoleLib.AxGlg();
             this.axGlg13 = new AxGlgoleLib.AxGlg();
             this.axGlg14 = new AxGlgoleLib.AxGlg();
             this.axGlg15 = new AxGlgoleLib.AxGlg();
             this.loopTimer = new System.Windows.Forms.Timer(this.components);
             this.voltageLabel = new System.Windows.Forms.Label();
             this.currentLabel = new System.Windows.Forms.Label();
+            this.pantographIndicator = new AxGlgoleLib.AxGlg();
+            this.axGlg12 = new AxGlgoleLib.AxGlg();
+            this.kvLabel = new System.Windows.Forms.Label();
+            this.hzLabel = new System.Windows.Forms.Label();
+            this.sysPowLabel = new System.Windows.Forms.Label();
+            this.percentLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg3)).BeginInit();
@@ -54,10 +59,11 @@ namespace BulletTrainGUI
             ((System.ComponentModel.ISupportInitialize)(this.axGlg7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg10)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.axGlg5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg13)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg14)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pantographIndicator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg12)).BeginInit();
             this.SuspendLayout();
             // 
             // axGlg1
@@ -66,14 +72,14 @@ namespace BulletTrainGUI
             this.axGlg1.Location = new System.Drawing.Point(21, 21);
             this.axGlg1.Name = "axGlg1";
             this.axGlg1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg1.OcxState")));
-            this.axGlg1.Size = new System.Drawing.Size(101, 183);
+            this.axGlg1.Size = new System.Drawing.Size(101, 193);
             this.axGlg1.TabIndex = 0;
             this.axGlg1.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg1_Input);
             // 
             // axGlg2
             // 
             this.axGlg2.Enabled = true;
-            this.axGlg2.Location = new System.Drawing.Point(21, 201);
+            this.axGlg2.Location = new System.Drawing.Point(21, 205);
             this.axGlg2.Name = "axGlg2";
             this.axGlg2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg2.OcxState")));
             this.axGlg2.Size = new System.Drawing.Size(101, 74);
@@ -83,7 +89,7 @@ namespace BulletTrainGUI
             // axGlg3
             // 
             this.axGlg3.Enabled = true;
-            this.axGlg3.Location = new System.Drawing.Point(21, 557);
+            this.axGlg3.Location = new System.Drawing.Point(21, 578);
             this.axGlg3.Name = "axGlg3";
             this.axGlg3.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg3.OcxState")));
             this.axGlg3.Size = new System.Drawing.Size(392, 284);
@@ -92,7 +98,7 @@ namespace BulletTrainGUI
             // axGlg4
             // 
             this.axGlg4.Enabled = true;
-            this.axGlg4.Location = new System.Drawing.Point(351, 109);
+            this.axGlg4.Location = new System.Drawing.Point(362, 112);
             this.axGlg4.Name = "axGlg4";
             this.axGlg4.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg4.OcxState")));
             this.axGlg4.Size = new System.Drawing.Size(51, 92);
@@ -102,7 +108,7 @@ namespace BulletTrainGUI
             // axGlg6
             // 
             this.axGlg6.Enabled = true;
-            this.axGlg6.Location = new System.Drawing.Point(21, 498);
+            this.axGlg6.Location = new System.Drawing.Point(21, 517);
             this.axGlg6.Name = "axGlg6";
             this.axGlg6.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg6.OcxState")));
             this.axGlg6.Size = new System.Drawing.Size(392, 53);
@@ -111,64 +117,55 @@ namespace BulletTrainGUI
             // axGlg7
             // 
             this.axGlg7.Enabled = true;
-            this.axGlg7.Location = new System.Drawing.Point(21, 301);
+            this.axGlg7.Location = new System.Drawing.Point(21, 315);
             this.axGlg7.Name = "axGlg7";
             this.axGlg7.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg7.OcxState")));
-            this.axGlg7.Size = new System.Drawing.Size(186, 168);
+            this.axGlg7.Size = new System.Drawing.Size(186, 175);
             this.axGlg7.TabIndex = 6;
             // 
             // axGlg8
             // 
             this.axGlg8.Enabled = true;
-            this.axGlg8.Location = new System.Drawing.Point(227, 301);
+            this.axGlg8.Location = new System.Drawing.Point(227, 315);
             this.axGlg8.Name = "axGlg8";
             this.axGlg8.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg8.OcxState")));
-            this.axGlg8.Size = new System.Drawing.Size(186, 168);
+            this.axGlg8.Size = new System.Drawing.Size(186, 175);
             this.axGlg8.TabIndex = 7;
             // 
             // axGlg10
             // 
             this.axGlg10.Enabled = true;
-            this.axGlg10.Location = new System.Drawing.Point(1100, 21);
+            this.axGlg10.Location = new System.Drawing.Point(871, 23);
             this.axGlg10.Name = "axGlg10";
             this.axGlg10.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg10.OcxState")));
-            this.axGlg10.Size = new System.Drawing.Size(293, 183);
+            this.axGlg10.Size = new System.Drawing.Size(131, 226);
             this.axGlg10.TabIndex = 9;
             this.axGlg10.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg10_Input);
-            // 
-            // axGlg5
-            // 
-            this.axGlg5.Enabled = true;
-            this.axGlg5.Location = new System.Drawing.Point(437, 21);
-            this.axGlg5.Name = "axGlg5";
-            this.axGlg5.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg5.OcxState")));
-            this.axGlg5.Size = new System.Drawing.Size(166, 180);
-            this.axGlg5.TabIndex = 11;
             // 
             // axGlg13
             // 
             this.axGlg13.Enabled = true;
-            this.axGlg13.Location = new System.Drawing.Point(686, 21);
+            this.axGlg13.Location = new System.Drawing.Point(718, 21);
             this.axGlg13.Name = "axGlg13";
             this.axGlg13.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg13.OcxState")));
-            this.axGlg13.Size = new System.Drawing.Size(163, 180);
+            this.axGlg13.Size = new System.Drawing.Size(57, 228);
             this.axGlg13.TabIndex = 13;
             this.axGlg13.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg13_Input);
             // 
             // axGlg14
             // 
             this.axGlg14.Enabled = true;
-            this.axGlg14.Location = new System.Drawing.Point(866, 21);
+            this.axGlg14.Location = new System.Drawing.Point(1086, 23);
             this.axGlg14.Name = "axGlg14";
             this.axGlg14.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg14.OcxState")));
-            this.axGlg14.Size = new System.Drawing.Size(214, 183);
+            this.axGlg14.Size = new System.Drawing.Size(131, 227);
             this.axGlg14.TabIndex = 14;
             this.axGlg14.Input += new AxGlgoleLib._DGlgEvents_InputEventHandler(this.axGlg14_Input);
             // 
             // axGlg15
             // 
             this.axGlg15.Enabled = true;
-            this.axGlg15.Location = new System.Drawing.Point(154, 70);
+            this.axGlg15.Location = new System.Drawing.Point(165, 73);
             this.axGlg15.Name = "axGlg15";
             this.axGlg15.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg15.OcxState")));
             this.axGlg15.Size = new System.Drawing.Size(160, 164);
@@ -181,10 +178,11 @@ namespace BulletTrainGUI
             // voltageLabel
             // 
             this.voltageLabel.AutoSize = true;
-            this.voltageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.voltageLabel.Location = new System.Drawing.Point(943, 209);
+            this.voltageLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.voltageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.voltageLabel.Location = new System.Drawing.Point(900, 254);
             this.voltageLabel.Name = "voltageLabel";
-            this.voltageLabel.Size = new System.Drawing.Size(50, 25);
+            this.voltageLabel.Size = new System.Drawing.Size(54, 26);
             this.voltageLabel.TabIndex = 17;
             this.voltageLabel.Text = "30.0";
             this.voltageLabel.Click += new System.EventHandler(this.voltageLabel_Click);
@@ -192,26 +190,91 @@ namespace BulletTrainGUI
             // currentLabel
             // 
             this.currentLabel.AutoSize = true;
-            this.currentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.currentLabel.Location = new System.Drawing.Point(1224, 209);
+            this.currentLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.currentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.currentLabel.Location = new System.Drawing.Point(1112, 254);
             this.currentLabel.Name = "currentLabel";
-            this.currentLabel.Size = new System.Drawing.Size(50, 25);
+            this.currentLabel.Size = new System.Drawing.Size(54, 26);
             this.currentLabel.TabIndex = 18;
             this.currentLabel.Text = "40.1";
             this.currentLabel.Click += new System.EventHandler(this.currentLabel_Click);
+            // 
+            // pantographIndicator
+            // 
+            this.pantographIndicator.Enabled = true;
+            this.pantographIndicator.Location = new System.Drawing.Point(786, 205);
+            this.pantographIndicator.Name = "pantographIndicator";
+            this.pantographIndicator.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("pantographIndicator.OcxState")));
+            this.pantographIndicator.Size = new System.Drawing.Size(45, 44);
+            this.pantographIndicator.TabIndex = 21;
+            // 
+            // axGlg12
+            // 
+            this.axGlg12.Enabled = true;
+            this.axGlg12.Location = new System.Drawing.Point(466, 21);
+            this.axGlg12.Name = "axGlg12";
+            this.axGlg12.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGlg12.OcxState")));
+            this.axGlg12.Size = new System.Drawing.Size(186, 284);
+            this.axGlg12.TabIndex = 25;
+            // 
+            // kvLabel
+            // 
+            this.kvLabel.AutoSize = true;
+            this.kvLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.kvLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.kvLabel.Location = new System.Drawing.Point(948, 254);
+            this.kvLabel.Name = "kvLabel";
+            this.kvLabel.Size = new System.Drawing.Size(38, 26);
+            this.kvLabel.TabIndex = 26;
+            this.kvLabel.Text = "kV";
+            // 
+            // hzLabel
+            // 
+            this.hzLabel.AutoSize = true;
+            this.hzLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.hzLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.hzLabel.Location = new System.Drawing.Point(1161, 254);
+            this.hzLabel.Name = "hzLabel";
+            this.hzLabel.Size = new System.Drawing.Size(39, 26);
+            this.hzLabel.TabIndex = 27;
+            this.hzLabel.Text = "Hz";
+            // 
+            // sysPowLabel
+            // 
+            this.sysPowLabel.AutoSize = true;
+            this.sysPowLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.sysPowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.sysPowLabel.Location = new System.Drawing.Point(713, 254);
+            this.sysPowLabel.Name = "sysPowLabel";
+            this.sysPowLabel.Size = new System.Drawing.Size(54, 26);
+            this.sysPowLabel.TabIndex = 28;
+            this.sysPowLabel.Text = "10.0";
+            // 
+            // percentLabel
+            // 
+            this.percentLabel.AutoSize = true;
+            this.percentLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.percentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.percentLabel.Location = new System.Drawing.Point(762, 254);
+            this.percentLabel.Name = "percentLabel";
+            this.percentLabel.Size = new System.Drawing.Size(32, 26);
+            this.percentLabel.TabIndex = 29;
+            this.percentLabel.Text = "%";
             // 
             // TrainGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(1405, 853);
+            this.ClientSize = new System.Drawing.Size(1269, 874);
+            this.Controls.Add(this.sysPowLabel);
+            this.Controls.Add(this.axGlg12);
+            this.Controls.Add(this.pantographIndicator);
             this.Controls.Add(this.currentLabel);
             this.Controls.Add(this.voltageLabel);
             this.Controls.Add(this.axGlg15);
             this.Controls.Add(this.axGlg14);
             this.Controls.Add(this.axGlg13);
-            this.Controls.Add(this.axGlg5);
             this.Controls.Add(this.axGlg10);
             this.Controls.Add(this.axGlg8);
             this.Controls.Add(this.axGlg7);
@@ -220,6 +283,9 @@ namespace BulletTrainGUI
             this.Controls.Add(this.axGlg3);
             this.Controls.Add(this.axGlg2);
             this.Controls.Add(this.axGlg1);
+            this.Controls.Add(this.kvLabel);
+            this.Controls.Add(this.hzLabel);
+            this.Controls.Add(this.percentLabel);
             this.Name = "TrainGUI";
             this.Text = "Bullet Train HMI";
             ((System.ComponentModel.ISupportInitialize)(this.axGlg1)).EndInit();
@@ -230,10 +296,11 @@ namespace BulletTrainGUI
             ((System.ComponentModel.ISupportInitialize)(this.axGlg7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg10)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.axGlg5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg13)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg14)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axGlg15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pantographIndicator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axGlg12)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,13 +316,18 @@ namespace BulletTrainGUI
         private AxGlgoleLib.AxGlg axGlg7;
         private AxGlgoleLib.AxGlg axGlg8;
         private AxGlgoleLib.AxGlg axGlg10;
-        private AxGlgoleLib.AxGlg axGlg5;
         private AxGlgoleLib.AxGlg axGlg13;
         private AxGlgoleLib.AxGlg axGlg14;
         private AxGlgoleLib.AxGlg axGlg15;
         private System.Windows.Forms.Timer loopTimer;
         private System.Windows.Forms.Label voltageLabel;
         private System.Windows.Forms.Label currentLabel;
+        private AxGlgoleLib.AxGlg pantographIndicator;
+        private AxGlgoleLib.AxGlg axGlg12;
+        private System.Windows.Forms.Label kvLabel;
+        private System.Windows.Forms.Label hzLabel;
+        private System.Windows.Forms.Label sysPowLabel;
+        private System.Windows.Forms.Label percentLabel;
     }
 }
 
