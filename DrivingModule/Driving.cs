f88b34344a0a221d3ffa2cd57dd8c570172b59e4
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2021-11-28 16:52:17 -0500

    Revert "Revert "Merge branch 'Driving-Module'""
    This reverts commit 617c5c2a1bee88d725a4a87988799eef8808d1f4.

---
+++ DrivingModule/Driving.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace DrivingModule
+{
+   public abstract class Driving
+    {
+      
+
+        protected void WriteFile(string filePath, string storeContents)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath, append: true))
+                {                  
+                    sw.WriteAsync(storeContents + ",");
+                    sw.Close();
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: File Not Found");
+                System.Environment.Exit(1);
+            }
+        }
+
+        protected string ReadFile(string filePath, string placeContents)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    placeContents = sr.ReadLine();
+                    sr.Close();
+                    return placeContents;
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: File Not Found");
+                System.Environment.Exit(1);
+            }
+            return "";
+        }      
+    }
+}
